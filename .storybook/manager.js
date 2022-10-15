fe20bf5ecf48d4f1295f9bc2a0dfedee577b0085
Eberson Silva <65686722+EbersonSilva@users.noreply.github.com>
2022-10-15 10:31:23 -0300

    Projeto Ignite Lab

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons'
+import { themes } from '@storybook/theming'
+// Configuração de tema escuro
+addons.setConfig({
+  theme: themes.dark
+})
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css'
+
+
+export function App() {
+  return (
+    <div>
+      <h1 className='font-bold text-2xl text-gray-500'>Hello World</h1>
+      <button className='bg-cyan-500 font-md  px-4 py-2 rounded hover:bg-cyan-300'>Enviar</button>
+    </div>
+    
+  )
+}
+
+
